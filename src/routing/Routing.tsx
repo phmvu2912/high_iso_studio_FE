d31960aa61e595f10f24be42d8f59e4f3629cbaa
phmvu2912 <phmvu2912@gmail.com>
2025-06-04 13:53:38 +0700

    Create base FE project

---
+++ src/routing/Routing.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+
+const Routing = () => {
+  return (
+    <>
+        <Routes>
+            <Route path="/" element={'hi'}>
+    
+            </Route>
+        </Routes>
+    </>
+  )
+}
+
+export default Routing
