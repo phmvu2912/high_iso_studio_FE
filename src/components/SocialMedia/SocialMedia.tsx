f4fe59f55521e492080cce9057a718cf5c248a31
phmvu2912 <phmvu2912@gmail.com>
2025-06-04 15:40:59 +0700

    feat: create SocialMedia component in client

---
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import { facebookIcon, instagramIcon, youtubeIcon } from "../../constants/client"
+
+const SocialMedia = () => {
+
+    const icons = [facebookIcon, instagramIcon, youtubeIcon];
+
+    console.log(icons)
+
+    return (
+        <div className="fixed top-1/3 right-4 mr-4 z-50">
+            <div className="flex flex-col gap-y-2">
+                {
+                    icons.map(({ link, path, altText }, index) => (
+                        <Link to={link} target="_blank" className="cursor-pointer" key={index}>
+                            <img src={path} alt={altText} width={50} />
+                        </Link>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default SocialMedia
